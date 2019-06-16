53c9318277061f06c81e1ebec75c2b55cc12f25b
Daniel Pak <Pakjuhnman@gmail.com>
2019-06-16 09:13:57 -0600

    Adding password action creator. Added to types file and imported to components and actions files.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import { EMAIL_CHANGED } from './types'
+import { EMAIL_CHANGED, PASSWORD_CHANGED } from './types'
 
 export const emailChanged = (text) => {
   return {
@@ -6,3 +6,10 @@ export const emailChanged = (text) => {
     payload: text
   };
 };
+
+export const passwordChanged = (text) => {
+  return {
+    type: PASSWORD_CHANGED,
+    payload: text
+  }
+}
