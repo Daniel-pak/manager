53b55baab7d4b31e46d4d626003691829c3f3f53
Daniel Pak <Pakjuhnman@gmail.com>
2019-06-18 07:34:30 -0600

    Added in create account function with firebase in loginUser action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,7 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from './types'
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED
+} from './types';
 import firebase from 'firebase';
 
 export const emailChanged = (text) => {
@@ -15,14 +18,26 @@ export const passwordChanged = (text) => {
   };
 };
 
-export const loginUser = ({ email, password }) => {
+export const loginUser = ({
+  email,
+  password
+}) => {
   return (dispatch) => {
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(user => {
         dispatch({
-          type: "LOGIN_USER_SUCCESS",
-          payload: user
-        });
-    });
+            type: "LOGIN_USER_SUCCESS",
+            payload: user
+          })
+          .catch(() => {
+            firebase.auth().createUserWithEmailAndPassword(email, password)
+              .then(user => {
+                dispatch({
+                  type: LOGIN_USER_SUCCESS,
+                  payload: user
+                });
+              })
+          })
+      });
   };
 };
--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,5 +1,5 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from '../actions/types';
-const INITIAL_STATE = { email: '', password: '' };
+import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS } from '../actions/types';
+const INITIAL_STATE = { email: '', password: '', user: null };
 
 export default (state = INITIAL_STATE, action) => {
   console.log(action);
@@ -9,6 +9,8 @@ export default (state = INITIAL_STATE, action) => {
       return { ...state, email: action.payload };
     case PASSWORD_CHANGED:
       return { ...state, password: action.payload };
+    case LOGIN_USER_SUCCESS:
+      return { ...state, user: action.payload };
     default:
       return state;
   }
