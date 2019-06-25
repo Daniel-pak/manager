6c55df5f5df9ae7c93c771cc1430041d229316e7
Daniel Pak <Pakjuhnman@gmail.com>
2019-06-24 20:09:13 -0600

    Added view tag back in for styling. Completed login reducer.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { connect } from 'react-redux';
 import { emailChanged, passwordChanged, loginUser } from '../actions';
-import { Card, CardSection, Input, Button } from './common';
+import { Card, CardSection, Input, Button, Spinner } from './common';
 
 class LoginForm extends Component {
   onEmailChange(text) {
@@ -17,6 +17,16 @@ class LoginForm extends Component {
     const { email, password } = this.props;
 
     this.props.loginUser({ email, password });
+  }
+
+  renderButton() {
+    if (this.props.loading) {
+      return <Spinner size="large" />
+    } else {
+      return(<Button onPress={this.onButtonPress.bind(this)}>
+        Login
+      </Button>);
+    }
   }
 
   renderError() {
@@ -56,11 +66,7 @@ class LoginForm extends Component {
         {this.renderError()}
 
         <CardSection>
-          <Button
-            onPress={this.onButtonPress.bind(this)}
-          >
-            Login
-          </Button>
+          {this.renderButton()}
         </CardSection>
       </Card>
     );
@@ -79,7 +85,8 @@ const mapStateToProps = state => {
   return {
     email: state.auth.email,
     password: state.auth.password,
-    error: state.auth.error
+    error: state.auth.error,
+    loading: state.auth.loading
   }
 }
 
