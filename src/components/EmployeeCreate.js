e389a8e841b193bb897763d414bf499b5a63c73d
Daniel Pak <Pakjuhnman@gmail.com>
2019-06-30 00:09:34 -0600

    Added employeeCreate actions, refactored reducer index file. Added new reducer tocombine reducer file.

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import { View, Text } from 'react-native';
 import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
