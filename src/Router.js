8775ffba9bcd690d121365b110f4991cbb1a3926
Daniel Pak <Pakjuhnman@gmail.com>
2019-06-29 23:15:03 -0600

    Added in new Scene and changed action

---
+++ src/Router.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Scene, Router } from 'react-native-router-flux';
+import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
+
+const RouterComponent = () => {
+    return (
+      <Router>
+        <Scene key="root"hideNavBar>
+            <Scene key="auth">
+              <Scene key="login" component={LoginForm} title="Please Log In" initial />
+            </Scene>
+            <Scene key="main">
+              <Scene key="employeeList" component={EmployeeList} title="Employees" />
+            </Scene>
+        </Scene>
+      </Router>
+    );
+};
+
+export default RouterComponent;
