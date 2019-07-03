72c6267ec11f614f3c1f1643e5b1cede9fad1ca7
Daniel Pak <Pakjuhnman@gmail.com>
2019-07-03 14:04:47 -0600

    Added in new Confirm component to common component. Added confirm modal component to employeeEdit form component.

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,7 +1,8 @@
 import {
   EMPLOYEE_UPDATE,
   EMPLOYEE_CREATE,
-  EMPLOYEES_FETCH_SUCCESS
+  EMPLOYEES_FETCH_SUCCESS,
+  EMPLOYEE_SAVE_SUCCESS
 } from './types';
 import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
@@ -40,9 +41,12 @@ export const employeesFetch = () => {
 export const employeeSave = ({ name, phone, shift, uid }) => {
   const { currentUser } = firebase.auth();
   
-  return () => {
+  return (dispatch) => {
     firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
       .set({ name, phone, shift })
-      .then(() => console.log('saved!'));
+      .then(() => {
+        dispatch({ type: EMPLOYEE_SAVE_SUCCESS })
+        Actions.employeeList();
+      });
   };
 };
--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -2,10 +2,13 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import EmployeeForm from './EmployeeForm';
+import Communications from 'react-native-communications';
 import { employeeUpdate, employeeSave } from '../actions';
-import { Card, CardSection, Button } from './common';
+import { Card, CardSection, Button, Confirm } from './common';
 
 class EmployeeEdit extends Component {
+  state = { showModal: false };
+  
   componentWillMount() {
     _.each(this.props.employee, (value, prop) => {
       this.props.employeeUpdate({ prop, value });
@@ -17,6 +20,12 @@ class EmployeeEdit extends Component {
       this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid });
   }   
   
+  onTextPress() {
+      const { phone, shift } = this.props;
+      
+      Communications.text(phone, `Your upcoming shift is on ${shift}`);
+  }  
+  
   render() {
     return(
       <Card>
@@ -26,6 +35,21 @@ class EmployeeEdit extends Component {
             Save Changes
           </Button>
         </CardSection>
+        <CardSection>
+          <Button onPress={this.onTextPress.bind(this)}>
+            Text Schedule
+          </Button>
+        </CardSection>
+        <CardSection>
+          <Button onPress={() => this.setState({ showModal: !this.state.showModal })}>
+            Delete
+          </Button>
+        </CardSection>
+        <Confirm
+            visible={this.state.showModal}
+        >
+          Are you sure you want to delete this?
+        </Confirm>
       </Card>
     );
   };
